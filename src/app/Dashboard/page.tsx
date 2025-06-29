349354b96dfcf93fe19d44a55d3a1f5d515ff869
Mahdi <mahd8767trfx@gmail.com>
2025-06-29 12:17:16 +0330

    last style of Dashbaord

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,21 +1,19 @@
 'use client'
 import Getuser from "../Hooks/Getuser";
 import styles from '../Dashboard/dashboard.module.scss'
+import { use } from "react";
 export default function Dashboard(){
     const {user,loading,error} = Getuser()
     return ( 
         <div className={styles.dashboard_container}>
-            <h2 className={styles.welcome_message}>👋 Hi {user?.name.first}، Wellcome</h2>
-            <img src={user?.picture.large} alt="User" className={styles.user_avatar} />
-            <h1 className={styles.user_name}>{user?.name.first}</h1>
-            <p className={styles.user_email}>{user?.email}</p>
-            <p className={styles.user_phone}>{user?.phone}</p>
-            <button
-                className={styles.logout_btn}
-            >
-                خروج
-            </button>
+        <div className={styles.user_card}>
+            <img src={user?.picture.large} alt={user?.name.first} className={styles.user_avatar} />
+            <div className={styles.user_name}>{user?.name.first}</div>
+            <div className={styles.user_email}>{user?.email}</div>
+            <div className={styles.user_phone}>{user?.phone}</div>
         </div>
+
+    </div>
      );
 }
  
